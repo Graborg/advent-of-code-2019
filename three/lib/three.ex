fe701106f2b8d974ed553bfbbd99ffbd3a1e6d93
Mikael Gråborg <mikael.graborg@iteam.se>
2019-12-05 01:31:48 +0100

    add solution to 3.2

--- three/lib/three.ex
+++ three/lib/three.ex
@@ -6,62 +6,96 @@ defmodule Three do
 
   end
 
-  def get_closest_intersection(wire_one, wire_two) do
-    set_1 = wire_one
-    |> get_wire_positions()
-    |> List.delete([0,0])
-    |> List.delete([0,0])
-    |> Enum.into(HashSet.new())
+  def get_proximity_to_closest_intersection(wire_one, wire_two) do
+    p1 = get_wire_positions(wire_one)
+    p2 = get_wire_positions(wire_two)
+
+    get_intersections(p1, p2)
+      |> get_closest_intersection()
+  end
+
+  def get_closest_intersection(intersections) do
+    intersections
+      |> Enum.map(&Enum.map(&1, fn x -> Kernel.abs(x) end))
+      |> Enum.map(&Enum.sum/1)
+      |> Enum.min()
+  end
+
+  def get_steps_to_closest_intersection(wire_one, wire_two) do
+    p1 = get_wire_positions(wire_one)
+    p2 = get_wire_positions(wire_two)
+
+    intersections = get_intersections(p1, p2)
+
+    get_closest_intersection_from_steps(intersections, p1, p2)
+  end
+
+  def get_closest_intersection_from_steps(intersections, positions1, positions2) do
+    intersections
+      |> Enum.map(fn position -> [get_steps_to_position(positions1, position), get_steps_to_position(positions2, position)] end)
+      |> Enum.map(&Enum.sum/1)
+      |> Enum.min()
+  end
+
+  def get_steps_to_position(wire, position) do
+    Enum.find_index(wire, fn wire_pos -> position == wire_pos end)
+    |> Kernel.+(1)
+  end
+
+  def get_intersections(wire_one, wire_two) do
+    set_1 = Enum.into(wire_one, MapSet.new())
 
     wire_two
-    |> get_wire_positions()
-    |> List.delete([0,0])
-    |> List.delete([0,0])
-    |> Enum.into(HashSet.new())
-    |> Set.intersection(set_1)
-    |> Enum.map(&Enum.map(&1, fn x -> Kernel.abs(x) end))
-    |> Enum.map(&Enum.sum/1)
-    |> Enum.min()
+    |> Enum.into(MapSet.new())
+    |> MapSet.intersection(set_1)
   end
 
   def get_wire_positions(wire) do
     wire
     |> String.split(",")
     |> Enum.reduce([[0,0]], &move_wire/2)
+    |> List.delete([0,0])
   end
 
   def move_wire("R" <> steps, positions) do
     steps_int = String.to_integer(steps)
     positions
       |> List.last()
-      |> (fn [x, y] -> positions ++ Enum.map(x..(x + steps_int), &([&1, y])) end).()
+      |> (fn [x, y] -> positions ++ Enum.map(x+1..(x + steps_int), &([&1, y])) end).()
   end
 
   def move_wire("L" <> steps, positions) do
     steps_int = String.to_integer(steps)
     positions
       |> List.last()
-      |> (fn [x, y] -> positions ++ Enum.map(x..(x - steps_int), &([&1, y])) end).()
+      |> (fn [x, y] -> positions ++ Enum.map(x-1..(x - steps_int), &([&1, y])) end).()
   end
 
   def move_wire("U" <> steps, positions) do
     steps_int = String.to_integer(steps)
     positions
       |> List.last()
-      |> (fn [x, y] -> positions ++ Enum.map(y..(y + steps_int), &([x, &1])) end).()
+      |> (fn [x, y] -> positions ++ Enum.map(y+1..(y + steps_int), &([x, &1])) end).()
   end
 
   def move_wire("D" <> steps, positions) do
     steps_int = String.to_integer(steps)
     positions
       |> List.last()
-      |> (fn [x, y] -> positions ++ Enum.map(y..(y - steps_int), &([x, &1])) end).()
+      |> (fn [x, y] -> positions ++ Enum.map(y-1..(y - steps_int), &([x, &1])) end).()
   end
 
   def run do
     [wire1, wire2] = get_puzzle_input()
-    IO.inspect(get_closest_intersection(wire1, wire2))
+    get_proximity_to_closest_intersection(wire1, wire2)
+  end
+
+  def run2 do
+    [wire1, wire2] = get_puzzle_input()
+
+    get_steps_to_closest_intersection(wire1, wire2)
   end
 end
 
-Three.run()
+# IO.inspect(Three.run())
+# IO.inspect(Three.run2())
--- three/test/three_test.exs
+++ three/test/three_test.exs
@@ -5,12 +5,26 @@ defmodule ThreeTest do
   test "test case one" do
     wire_one = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
     wire_two = "U62,R66,U55,R34,D71,R55,D58,R83"
-    assert Three.get_closest_intersection(wire_one, wire_two) == 159
+    assert Three.get_proximity_to_closest_intersection(wire_one, wire_two) == 159
   end
 
   test "test case two" do
     wire_one = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
     wire_two = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
-    assert Three.get_closest_intersection(wire_one, wire_two) == 135
+    assert Three.get_proximity_to_closest_intersection(wire_one, wire_two) == 135
+  end
+
+  test "test case three" do
+    wire_one = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+    wire_two = "U62,R66,U55,R34,D71,R55,D58,R83"
+    assert Three.get_steps_to_closest_intersection(wire_one, wire_two) == 610
+
+  end
+
+  test "test case four" do
+    wire_one = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+    wire_two = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+    assert Three.get_steps_to_closest_intersection(wire_one, wire_two) == 410
+
   end
 end
