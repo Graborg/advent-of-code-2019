ead0c71611fe3632a2903376e203088d1ffd1911
Mikael Gråborg <mikael.graborg@iteam.se>
2019-12-13 11:35:32 +0100

    feat: 6.2

--- six/lib/six.ex
+++ six/lib/six.ex
@@ -5,23 +5,24 @@ defmodule Six do
   end
 
   def get_nr_orbits(data) do
-    map = data
-      |> Enum.reduce(%{}, &insert_in_map/2)
-      |> IO.inspect()
+    map = data |> Enum.reduce(%{}, &insert_in_map/2)
 
-    orbitees = Map.values(map) |> List.flatten()
-    leaf_orbiter = Map.keys(map)
-      |> IO.inspect()
+    Map.keys(map)
       |> Enum.map( fn orbiter -> count_orbitees(orbiter, map)end)
+      |> Enum.map(&List.last/1)
+      |> Enum.map(&(elem(&1,0)))
       |> Enum.sum()
-      |> IO.inspect()
   end
 
-  def count_orbitees(orbiter, map) do
+  def count_orbitees(orbiter, map), do: count_orbitees(orbiter, map, 0)
+
+  def count_orbitees(orbiter, map, counter) do
     Map.get(map, orbiter, [])
-      |> Enum.map(fn orbiter -> count_orbitees(orbiter, map)end)
-      |> Enum.sum()
-      |> Kernel.+(Map.get(map, orbiter, []) |> Enum.count())
+      |> Enum.flat_map(fn orbiter -> count_orbitees(orbiter, map, counter + 1) end)
+      |> case do
+        [] -> [{counter, orbiter}]
+        list -> List.insert_at(list, 0, {counter, orbiter})
+      end
   end
 
   def insert_in_map(orbit, map) do
@@ -33,8 +34,37 @@ defmodule Six do
       end)
     end).()
   end
+
   def get_nr_orbits_for_map_data() do
     get_puzzle_input()
       |> get_nr_orbits()
   end
+
+  def get_min_path_to_santa() do
+    get_puzzle_input()
+      |> get_min_path_to_santa()
+  end
+
+  def get_min_path_to_santa(orbit_map) do
+    orbit_map
+      |> Enum.reduce(%{}, &insert_in_map/2)
+      |> get_denominator_orbitee("YOU", "SAN")
+  end
+
+  def get_denominator_orbitee(orbit_map, orbiter1, orbiter2) do
+    orbits2 = count_orbitees(orbiter2, orbit_map)
+
+    count_orbitees(orbiter1, orbit_map)
+      |> Enum.reduce_while({nil, nil}, fn
+          o1, {_, nil} ->
+            Enum.find(orbits2, fn o2 -> elem(o2, 1) == elem(o1, 1) end)
+              |> case do o2 -> {:cont, {o1, o2}} end
+          _, ans -> {:halt, ans}
+        end)
+    |> Tuple.to_list()
+    |> Enum.map(fn tuple -> elem(tuple, 0) end)
+    |> Enum.map(&(&1 - 1)) # Remove own orbit
+    |> Enum.sum()
+  end
+
 end
--- six/test/six_test.exs
+++ six/test/six_test.exs
@@ -7,6 +7,15 @@ defmodule SixTest do
   end
 
   test "calculates orbits from input" do
-    assert Six.get_nr_orbits_for_map_data() == 42
+    assert Six.get_nr_orbits_for_map_data() == 270768
+  end
+
+  test "get min path to santa" do
+    orbit_map = ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN"]
+    assert Six.get_min_path_to_santa(orbit_map) == 4
+  end
+
+  test "get min path to santa real map" do
+    assert Six.get_min_path_to_santa() == 451
   end
 end
