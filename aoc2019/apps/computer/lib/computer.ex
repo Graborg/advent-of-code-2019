8fd8f0f62e306ed05d8052d83dd94a97b2b4eab9
Mikael Gråborg <mikael.graborg@iteam.se>
2019-12-23 15:02:12 +0100

    refactor: simplify computer

--- aoc2019/apps/computer/lib/computer.ex
+++ aoc2019/apps/computer/lib/computer.ex
@@ -1,18 +1,10 @@
 defmodule Computer do
-  def execute({:exit, computer}), do: %{:status => :exit, :computer => computer}
-
-  def execute({:add, arg1, arg2, storage_pos, computer}) do
-    ans = arg1 + arg2
-    store(computer, ans, storage_pos)
-  end
-
-  def execute({:multiply, arg1, arg2, storage_pos, computer}) do
-    ans = arg1 * arg2
-    store(computer, ans, storage_pos)
-  end
+  defp execute({:exit, computer}), do: %{:status => :exit, :computer => computer}
+  defp execute({:add, arg1, arg2, storage_pos, computer}), do: store(computer, arg1 + arg2, storage_pos)
+  defp execute({:multiply, arg1, arg2, storage_pos, computer}), do: store(computer, arg1 * arg2, storage_pos)
 
   # Take user input and store at storage_pos
-  def execute({:store_user_input, storage_pos, computer}) do
+  defp execute({:store_user_input, storage_pos, computer}) do
     [user_instruction | rest_of_instructions] = computer[:user_input]
 
     computer
@@ -21,45 +13,45 @@ defmodule Computer do
   end
 
   # print the value
-  def execute({:output, value, computer}) do
-    IO.puts("OUTPUT! #{value}\n\n\n")
-    %{ :status => :output, :computer => computer, :value => value}
+  defp execute({:output, value, computer}) do
+    value |> IO.inspect(label: "OUTPUT!")
+    %{ :status => :ok, :computer => Map.put(computer, :latest_output, value)}
   end
 
 
   # jump to jump_position if value is non-zero
-  def execute({:jump_if_non_zero, 0, _jump_position, computer}), do: %{ :status => :ok, :computer => computer}
-  def execute({:jump_if_non_zero, _value, jump_position, computer}), do: %{ :status => :ok, :computer => Map.update!(computer, :index, fn _ -> jump_position end)}
+  defp execute({:jump_if_non_zero, 0, _jump_position, computer}), do: %{ :status => :ok, :computer => computer}
+  defp execute({:jump_if_non_zero, _value, jump_position, computer}), do: %{ :status => :ok, :computer => Map.update!(computer, :index, fn _ -> jump_position end)}
 
-  # jump to jump_position if value1 is zero
-  def execute({:jump_if_zero, 0, jump_position, computer}), do: %{ :status => :ok, :computer => Map.update!(computer, :index, fn _ -> jump_position end)}
-  def execute({:jump_if_zero, _value, _jump_position, computer}), do: %{ :status => :ok, :computer => computer}
+  # jump to jump_position if value is zero
+  defp execute({:jump_if_zero, 0, jump_position, computer}), do: %{ :status => :ok, :computer => Map.update!(computer, :index, fn _ -> jump_position end)}
+  defp execute({:jump_if_zero, _value, _jump_position, computer}), do: %{ :status => :ok, :computer => computer}
 
   # 1 in memory if v1 less than v2 (otherwise 0)
-  def execute({:store_1_if_lt, v1, v2, storage_pos, computer}) when v1 < v2, do: store(computer, 1, storage_pos)
-  def execute({:store_1_if_lt, _, _, storage_pos, computer}), do: store(computer, 0, storage_pos)
+  defp execute({:store_1_if_lt, v1, v2, storage_pos, computer}) when v1 < v2, do: store(computer, 1, storage_pos)
+  defp execute({:store_1_if_lt, _, _, storage_pos, computer}), do: store(computer, 0, storage_pos)
 
   # 1 in memory v1 equals v2 (otherwise 0)
-  def execute({:store_1_if_eq, v1, v2, storage_pos, computer}) when v1 == v2, do: store(computer, 1, storage_pos)
-  def execute({:store_1_if_eq, _, _, storage_pos, computer}), do: store(computer, 0, storage_pos)
+  defp execute({:store_1_if_eq, v1, v2, storage_pos, computer}) when v1 == v2, do: store(computer, 1, storage_pos)
+  defp execute({:store_1_if_eq, _, _, storage_pos, computer}), do: store(computer, 0, storage_pos)
 
-  def execute({:increment_relative_base, value, computer}), do: %{ :status => :ok, :computer => Map.update!(computer, :relative_base, &(&1 + value))}
+  defp execute({:incr_relative_base, value, computer}), do: %{ :status => :ok, :computer => Map.update!(computer, :relative_base, &(&1 + value))}
 
-  def store(computer, value, storage_pos) when is_integer(value) and is_integer(storage_pos) do
+  defp store(computer, value, storage_pos) when is_integer(value) and is_integer(storage_pos) do
     updated_computer = Map.update!(computer, :memory, fn memory -> List.replace_at(memory, storage_pos, value) end)
     %{:status => :ok, :computer => updated_computer}
   end
 
-  def opcode_to_readable(1), do: {:add, 2}
-  def opcode_to_readable(2), do: {:multiply, 2}
-  def opcode_to_readable(3), do: {:store_user_input, 0}
-  def opcode_to_readable(4), do: {:output, 1}
-  def opcode_to_readable(5), do: {:jump_if_non_zero, 2}
-  def opcode_to_readable(6), do: {:jump_if_zero, 2}
-  def opcode_to_readable(7), do: {:store_1_if_lt, 2}
-  def opcode_to_readable(8), do: {:store_1_if_eq, 2}
-  def opcode_to_readable(9), do: {:increment_relative_base, 1}
-  def opcode_to_readable(99), do: {:exit, 0}
+  defp opcode_to_readable(1), do: {:add, 2}
+  defp opcode_to_readable(2), do: {:multiply, 2}
+  defp opcode_to_readable(3), do: {:store_user_input, 0}
+  defp opcode_to_readable(4), do: {:output, 1}
+  defp opcode_to_readable(5), do: {:jump_if_non_zero, 2}
+  defp opcode_to_readable(6), do: {:jump_if_zero, 2}
+  defp opcode_to_readable(7), do: {:store_1_if_lt, 2}
+  defp opcode_to_readable(8), do: {:store_1_if_eq, 2}
+  defp opcode_to_readable(9), do: {:incr_relative_base, 1}
+  defp opcode_to_readable(99), do: {:exit, 0}
 
   def get_args_from_computer(%{:memory => memory, :index => index} = computer, no_of_args) do
     args = memory |> Enum.slice(index, no_of_args)
@@ -68,7 +60,7 @@ defmodule Computer do
 
   def inc_cursor(computer, step), do: Map.update!(computer, :index, &(&1 + step))
 
-  def get_arguments(computer) do
+  def get_next_instruction(computer) do
     {modes, opcode} = computer[:memory]
       |> Enum.at(computer[:index])
       |> Integer.digits()
@@ -76,37 +68,40 @@ defmodule Computer do
     computer = inc_cursor(computer, 1)
 
     opcode
-    |> Integer.undigits()
-    |> opcode_to_readable()
-    |> IO.inspect(label: "OPcode")
-    |> case do
-      {code, no_of_input_args} when code in [:add, :multiply, :store_1_if_lt, :store_1_if_eq] ->
-        {[arg1, arg2], computer} = get_args_from_computer(computer, 2)
-           |> arguments_to_values(Enum.slice(modes, -no_of_input_args, no_of_input_args))
-        {store_in_position, computer} = computer
-          |> get_args_from_computer(1)
-          |> arguments_to_output_pos(Enum.at(modes, -3, 0))
-        {code, arg1, arg2, store_in_position, computer}
-      {:store_user_input, _} ->
-        {store_in_position, computer} = computer
-          |> get_args_from_computer(1)
-          |> arguments_to_output_pos(List.first(modes))
+      |> Integer.undigits()
+      |> opcode_to_readable()
+      |> IO.inspect(label: "OPcode")
+      |> case do
+        {code, no_of_input_args} when code in [:add, :multiply, :store_1_if_lt, :store_1_if_eq] ->
+          {[arg1, arg2], computer} = computer
+            |> get_args_from_computer(2)
+            |> arguments_to_values(Enum.slice(modes, -no_of_input_args, no_of_input_args))
 
-          {:store_user_input, store_in_position, computer}
-      {code, no_of_input_args} when code in [:output, :increment_relative_base] ->
-        {[value], computer} = computer
-        |> get_args_from_computer(no_of_input_args)
-        |> arguments_to_values(Enum.slice(modes, -no_of_input_args, no_of_input_args))
+          {store_in_position, computer} = computer
+            |> get_args_from_computer(1)
+            |> arguments_to_output_pos(Enum.at(modes, -3, 0))
 
-        {code, value, computer}
-      {code, no_of_input_args} when code in [:jump_if_non_zero, :jump_if_zero] ->
-        {[number_to_compare, jump_pos], computer} = computer
-          |> get_args_from_computer(no_of_input_args)
-          |> arguments_to_values(Enum.slice(modes, -no_of_input_args, no_of_input_args))
+          {code, arg1, arg2, store_in_position, computer}
+        {:store_user_input, _} ->
+          {store_in_position, computer} = computer
+            |> get_args_from_computer(1)
+            |> arguments_to_output_pos(List.first(modes))
 
-        {code, number_to_compare, jump_pos, computer}
-      {:exit, _} -> {:exit, computer}
-      end
+            {:store_user_input, store_in_position, computer}
+        {code, no_of_input_args} when code in [:output, :incr_relative_base] ->
+          {[value], computer} = computer
+            |> get_args_from_computer(no_of_input_args)
+            |> arguments_to_values(Enum.slice(modes, -no_of_input_args, no_of_input_args))
+
+          {code, value, computer}
+        {code, no_of_input_args} when code in [:jump_if_non_zero, :jump_if_zero] ->
+          {[number_to_compare, jump_pos], computer} = computer
+            |> get_args_from_computer(no_of_input_args)
+            |> arguments_to_values(Enum.slice(modes, -no_of_input_args, no_of_input_args))
+
+          {code, number_to_compare, jump_pos, computer}
+        {:exit, _} -> {:exit, computer}
+        end
   end
 
   def arguments_to_output_pos({[arg], computer}, nil), do: arguments_to_output_pos({[arg], computer}, 0)
@@ -133,25 +128,24 @@ defmodule Computer do
     {values, computer}
   end
 
-  def extend_memory(memory), do: Enum.concat(memory, List.duplicate(0, 10000000))
+  defp extend_memory(memory), do: Enum.concat(memory, List.duplicate(0, 10000000))
 
-  def init_computer(memory, user_input) do
+  defp init_computer(memory, user_input) do
     Map.new()
       |> Map.put(:memory, memory |> extend_memory())
       |> Map.put(:user_input, user_input)
       |> Map.put(:index, 0)
       |> Map.put(:relative_base, 0)
   end
-  def calculate(memory, user_input), do: calculate(init_computer(memory, user_input))
 
-  def calculate(computer) do
+  def run(memory, user_input), do: run(init_computer(memory, user_input))
+  def run(computer) do
     computer
-      |> get_arguments()
+      |> get_next_instruction()
       |> execute()
       |> (fn
-        %{ :status => :ok, :computer => computer } -> calculate(computer)
-        %{ :status => :output} = output -> {output[:computer], output[:value]}
-        %{ :status => :exit} = output -> output[:computer]
+        %{ :status => :ok, :computer => computer } -> run(computer)
+        %{ :status => :exit, :computer => computer} -> {computer[:memory], computer[:latest_output]}
       end).()
   end
 end
--- aoc2019/apps/computer/test/computer_test.exs
+++ aoc2019/apps/computer/test/computer_test.exs
@@ -2,41 +2,41 @@ defmodule ComputerTest do
   use ExUnit.Case
 
   test "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9 with input 0 is zero" do
-    assert elem(Computer.calculate([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], [0]), 1) == 0
+    assert elem(Computer.run([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], [0]), 1) == 0
   end
 
   test "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9 with input 1 is not zero" do
-    assert elem(Computer.calculate([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], [5]), 1) == 1
+    assert elem(Computer.run([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], [5]), 1) == 1
   end
 
   test "3,3,1105,-1,9,1101,0,0,12,4,12,99,1 with input 0 is zero" do
-    assert elem(Computer.calculate([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], [0]), 1) == 0
+    assert elem(Computer.run([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], [0]), 1) == 0
   end
 
   test "3,3,1105,-1,9,1101,0,0,12,4,12,99,1 with input 1 is not zero" do
-    assert elem(Computer.calculate([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], [5]), 1) == 1
+    assert elem(Computer.run([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], [5]), 1) == 1
   end
 
   test "longer input can handle less than" do
-    assert elem(Computer.calculate([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], [7]), 1) == 999
+    assert elem(Computer.run([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], [7]), 1) == 999
   end
 
   test "longer input can handle equals" do
-    assert elem(Computer.calculate([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], [8]), 1) == 1000
+    assert elem(Computer.run([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], [8]), 1) == 1000
   end
 
   test "longer input can handle greater than" do
-    assert elem(Computer.calculate([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], [50]), 1) == 1001
+    assert elem(Computer.run([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], [50]), 1) == 1001
   end
 
   test "produces quine" do
     input_memory = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
-    {computer, _} = Computer.calculate(input_memory, [])
-    assert Enum.slice(computer[:memory], 0, Enum.count(input_memory)) == input_memory
+    {memory, _} = Computer.run(input_memory, [])
+    assert Enum.slice(memory, 0, Enum.count(input_memory)) == input_memory
   end
 
   test "produces 16 digit number" do
-    { _, value } = Computer.calculate([1102,34915192,34915192,7,4,7,99,0], [])
+    { _, value } = Computer.run([1102,34915192,34915192,7,4,7,99,0], [])
     value
       |> Integer.digits()
       |> Enum.count()
@@ -45,7 +45,7 @@ defmodule ComputerTest do
   end
 
   test "produce biggest number in middle" do
-    {_, value } = Computer.calculate([104,1125899906842624,99], [])
+    {_, value } = Computer.run([104,1125899906842624,99], [])
     assert value == 1125899906842624
   end
 end
