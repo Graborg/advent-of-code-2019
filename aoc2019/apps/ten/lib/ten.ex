c418da318fabeaab3d28702adf87c52ccd6ad292
Mikael Gråborg <mikael.graborg@iteam.se>
2019-12-26 13:31:49 +0100

    feat: 10.1

--- aoc2019/apps/ten/lib/ten.ex
+++ aoc2019/apps/ten/lib/ten.ex
@@ -1,22 +1,31 @@
 defmodule Ten do
-  def map_to_detections(input) do
+
+  def map_to_detections() do
+    Utilities.get_puzzle_input()
+      |> map_to_detections(36)
+  end
+  def map_to_detections(input, map_size) do
     input
     |> String.replace(~r{\s}, "")
     |> String.split("", trim: true)
-    |> get_asteroids_in_blockable_paths()
+    |> get_asteroids_in_blockable_paths(map_size)
   end
-  def get_asteroid_coordinates(asteroid_map) do
+
+  def get_asteroid_coordinates(asteroid_map, map_size) do
     asteroid_map
     |> Enum.with_index()
     |> Enum.filter(fn {sign, _index} -> sign == "#" end)
     |> Enum.map(fn {_, index} -> index end)
-    |> Enum.map(&index_to_coordinates/1)
+    |> Enum.map(&(index_to_coordinates(&1, map_size)))
   end
-  def get_asteroids_in_blockable_paths(asteroid_map) do
-    get_asteroid_coordinates(asteroid_map)
-      |> Enum.map(fn {x_1, y_1} = asteroid_1 ->
-        IO.inspect(asteroid_1, label: "asteroid")
-        get_asteroid_coordinates(asteroid_map)
+
+  def get_asteroids_in_blockable_paths(asteroid_map, map_size) do
+    asteroids = get_asteroid_coordinates(asteroid_map, map_size)
+
+    asteroids
+      |> Enum.map(fn {x_1, y_1} ->
+        # IO.inspect(asteroid_1, label: "\nasteroid")
+        asteroids
           |> Enum.map( fn {x_2, y_2} -> {x_2 - x_1, y_2 - y_1}  end)
           |> Enum.filter(fn e -> e != {0, 0} end)
           # |> IO.inspect()
@@ -26,27 +35,35 @@ defmodule Ten do
             smallest_y_changer = Integer.floor_div(y_changer, gcd)
             # IO.inspect({smallest_x_changer, smallest_y_changer}, label: "changer")
             # IO.inspect({x_1 + x_changer + smallest_x_changer, y_1 + y_changer + smallest_y_changer }, label: "startpos")
-            get_asteroids_in_blockable_paths(asteroid_map, {x_1 + x_changer + smallest_x_changer, y_1 + y_changer + smallest_y_changer }, {x_changer, y_changer}, [])
+            get_asteroids_in_blockable_paths(asteroid_map, {x_1 + x_changer + smallest_x_changer, y_1 + y_changer + smallest_y_changer }, {smallest_x_changer, smallest_y_changer}, [], map_size)
             |> Enum.filter(fn e -> e != nil end)
           end)
           |> Enum.filter(fn e -> !Enum.empty?(e) end)
           |> List.flatten()
+          # |> IO.inspect(label: "blocking things")
           |> Enum.into(MapSet.new())
-          |> IO.inspect(label: "blocking things")
+          # |> IO.inspect(label: "blocking things")
           |> Enum.count()
         end)
-        |> Enum.map(fn blocked_asteroids -> 9 - blocked_asteroids end)
-        |> IO.inspect()
+        |> Enum.map(fn blocked_asteroids -> Enum.count(asteroids) - 1 - blocked_asteroids end)
+        |> Enum.zip(asteroids)
+        # |> IO.inspect()
+        |> Enum.sort(fn {v1, _}, {v2, _} -> v1 > v2 end)
+        |> List.first()
   end
 
-  def get_asteroids_in_blockable_paths(asteroid_map, {x, y}, _, asteroids) when x < 0 or y < 0 or x > 4 or y > 4, do: asteroids
-  def get_asteroids_in_blockable_paths(asteroid_map, {x, y}, {x_changer, y_changer} = changer, asteroids_blocked) do
-    new_asteroid_blocked = asteroids_in_position(asteroid_map, {x, y})
+  def get_asteroids_in_blockable_paths(_asteroid_map, {x, y}, _, asteroids, map_size) when x < 0 or y < 0 or x >= map_size or y >= map_size, do: asteroids
+  def get_asteroids_in_blockable_paths(asteroid_map, {x, y}, {x_changer, y_changer} = changer, asteroids_blocked, map_size) do
+    new_asteroid_blocked = asteroids_in_position(asteroid_map, {x, y}, map_size)
+    # IO.inspect(new_asteroid_blocked, label: "found")
+    # IO.inspect({x, y}, label: "cords")
+    # IO.inspect(changer, label: "cords")
     asteroid_map
-      |> get_asteroids_in_blockable_paths({x + x_changer, y + y_changer}, changer, List.insert_at(asteroids_blocked, -1, new_asteroid_blocked))
+      |> get_asteroids_in_blockable_paths({x + x_changer, y + y_changer}, changer, List.insert_at(asteroids_blocked, -1, new_asteroid_blocked), map_size)
   end
-  def asteroids_in_position(asteroid_map, coordinates) do
-    Enum.at(asteroid_map, coordinates_to_index(coordinates))
+
+  def asteroids_in_position(asteroid_map, coordinates, map_size) do
+    Enum.at(asteroid_map, coordinates_to_index(coordinates, map_size))
       |> Kernel.==("#")
       |> case do
         true -> coordinates
@@ -54,6 +71,6 @@ defmodule Ten do
       end
   end
 
-  def coordinates_to_index({x, y}), do: x + y * 5
-  def index_to_coordinates(index), do: {rem(index, 5), div(index, 5)}
+  def coordinates_to_index({x, y}, map_length), do: x + y * map_length
+  def index_to_coordinates(index, map_length), do: {rem(index, map_length), div(index, map_length)}
 end
--- aoc2019/apps/ten/test/ten_test.exs
+++ aoc2019/apps/ten/test/ten_test.exs
@@ -3,42 +3,42 @@ defmodule TenTest do
   doctest Ten
 
   test "can gets index of 1,1" do
-    assert Ten.coordinates_to_index({1, 1}) == 6
+    assert Ten.coordinates_to_index({1, 1}, 5) == 6
   end
 
   test "can gets index of 0,1" do
-    assert Ten.coordinates_to_index({0, 1}) == 5
+    assert Ten.coordinates_to_index({0, 1}, 5) == 5
   end
 
   test "can gets index of 3,3" do
-    assert Ten.coordinates_to_index({3, 3}) == 18
+    assert Ten.coordinates_to_index({3, 3}, 5) == 18
   end
 
   test "can gets index of 0,0" do
-    assert Ten.coordinates_to_index({0, 0}) == 0
+    assert Ten.coordinates_to_index({0, 0}, 5) == 0
   end
 
   test "can gets index of 2,0" do
-    assert Ten.coordinates_to_index({2, 0}) == 2
+    assert Ten.coordinates_to_index({2, 0}, 5) == 2
   end
 
   test "can gets index of 4" do
-    assert Ten.index_to_coordinates(4) == {4, 0}
+    assert Ten.index_to_coordinates(4, 5) == {4, 0}
   end
 
   test "can gets index of 15" do
-    assert Ten.index_to_coordinates(18) == {3, 3}
+    assert Ten.index_to_coordinates(18, 5) == {3, 3}
   end
 
   test "can gets index of 0" do
-    assert Ten.index_to_coordinates(0) == {0, 0}
+    assert Ten.index_to_coordinates(0, 5) == {0, 0}
   end
 
   test "can gets index of 2" do
-    assert Ten.index_to_coordinates(2) == {2, 0}
+    assert Ten.index_to_coordinates(2, 5) == {2, 0}
   end
 
-  test "can find astroids" do
+  test "can find astroids 4x4" do
     input = """
       .#..#
       .....
@@ -47,7 +47,92 @@ defmodule TenTest do
       ...##
     """
 
-    res = Ten.map_to_detections(input)
-    assert res == [7, 7, 6, 7, 7, 7, 5, 7, 8, 7]
+    res = Ten.map_to_detections(input, 5)
+    assert res == {8, {3, 4}}
+  end
+  test "can find astroids in big" do
+    input = """
+    ......#.#.
+    #..#.#....
+    ..#######.
+    .#.#.###..
+    .#..#.....
+    ..#....#.#
+    #..#....#.
+    .##.#..###
+    ##...#..#.
+    .#....####
+    """
+
+    res = Ten.map_to_detections(input, 10)
+    assert res == {33, {5,8}}
+  end
+  test "can find astroids in bigg" do
+    input = """
+    #.#...#.#.
+    .###....#.
+    .#....#...
+    ##.#.#.#.#
+    ....#.#.#.
+    .##..###.#
+    ..#...##..
+    ..##....##
+    ......#...
+    .####.###.
+    """
+
+    res = Ten.map_to_detections(input, 10)
+    assert res == {35, {1,2}}
+  end
+
+  test "can find astroids in biggg" do
+    input = """
+    .#..#..###
+    ####.###.#
+    ....###.#.
+    ..###.##.#
+    ##.##.#.#.
+    ....###..#
+    ..#.#..#.#
+    #..#.#.###
+    .##...##.#
+    .....#.#..
+    """
+
+    res = Ten.map_to_detections(input, 10)
+    assert res == {41, {6,3}}
+  end
+
+  test "can find astroids in biggest" do
+    input = """
+    .#..##.###...#######
+    ##.############..##.
+    .#.######.########.#
+    .###.#######.####.#.
+    #####.##.#.##.###.##
+    ..#####..#.#########
+    ####################
+    #.####....###.#.#.##
+    ##.#################
+    #####.##.###..####..
+    ..######..##.#######
+    ####.##.####...##..#
+    .#####..#.######.###
+    ##...#.##########...
+    #.##########.#######
+    .####.#.###.###.#.##
+    ....##.##.###..#####
+    .#.#.###########.###
+    #.#.#.#####.####.###
+    ###.##.####.##.#..##
+    """
+
+    res = Ten.map_to_detections(input, 20)
+    assert res == {210, {11,13}}
+  end
+
+  test "can find astroids in challenge" do
+    res = Ten.map_to_detections()
+    assert res == {276, {17, 22}}
   end
 end
